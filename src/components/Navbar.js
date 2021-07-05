b6fdcbed03607eda3578178c7882b49d98500a27
Akash3444 <akki.moradiya007@gmail.com>
2021-07-05 02:43:10 +0000

    add icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-import {
-	AiOutlineHome,
-	AiFillHome,
-	AiOutlineMessage,
-	AiOutlineHeart,
-	AiOutlinePlusCircle,
-} from "react-icons/ai";
+import { AiOutlineHome } from "react-icons/ai";
 import { IoSearchOutline } from "react-icons/io5";
 import { useHistory } from "react-router-dom";
 import { CreatePost } from "./CreatePost";
 import { Search } from "./Search";
+import FillHome from "./icons/FillHome";
+import OutlineAdd from "./icons/OutlineAdd";
+import OutlineExplore from "./icons/OutlineExplore";
+import OutlineHeart from "./icons/OutlineHeart";
 
 export const Navbar = () => {
 	const history = useHistory();
@@ -29,19 +27,11 @@ export const Navbar = () => {
 
 					<div className='flex items-center space-x-5 text-gray-600'>
 						<div className='cursor-pointer' onClick={() => history.push("/")}>
-							{pathname === "/" ? (
-								<AiFillHome size={25} className='text-gray-800' />
-							) : (
-								<AiOutlineHome size={25} />
-							)}
+							{pathname === "/" ? <FillHome /> : <AiOutlineHome size={25} />}
 						</div>
-						<AiOutlinePlusCircle
-							size={25}
-							className='cursor-pointer'
-							onClick={() => setIsOpen(true)}
-						/>
-						<AiOutlineMessage size={25} className='cursor-pointer' />
-						<AiOutlineHeart size={25} className='cursor-pointer' />
+						<OutlineAdd />
+						<OutlineExplore />
+						<OutlineHeart />
 						<IoSearchOutline
 							size={25}
 							className='cursor-pointer'
