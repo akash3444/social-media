b7fde6ffa77f2d8bae13ff3753056c910b9490d5
Akash3444 <akashmoradiya3444@gmail.com>
2021-09-05 15:03:44 +0530

    create activity component

--- src/components/navbar/ActivityFeed.js
+++ src/components/navbar/ActivityFeed.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { useQuery } from '@apollo/client';
 import { Popover } from "@headlessui/react";
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import fromUnixTime from 'date-fns/fromUnixTime';
 import OutlineHeart from "../icons/OutlineHeart";
 import { GET_ACTIVITY } from "../../queries";
 import { useUserData } from "../../hooks/useUserData";
+import Activity from './Activity';
 
 const ActivityFeed = () => {
 	const { token } = useUserData();
@@ -23,23 +22,14 @@ const ActivityFeed = () => {
 			</Popover.Button>
 
 			<Popover.Panel
-				className='absolute -right-4 border h-96 w-96 bg-white shadow-xl rounded-lg'
+				className='absolute -right-4 border h-96 w-96 bg-white shadow-xl rounded-lg overflow-scroll'
 				style={{ top: "calc(100% + .5rem)" }}>
 				<div>
 				{loading && "Loading..."}
 				{error && "Error" + error.message}
-				{data && data.activities.map(({ username, text, profilePicture, createdAt}) => (
-					<div className="flex items-center p-4">
-						<div>
-							<img src={profilePicture ? profilePicture : 'https://picsum.photos/100/100'} alt={username} className="h-12 w-12 rounded-full ring-1 ring-offset-2 mr-5"/>
-						</div>
-						<div>
-							<b>{username}</b>
-							<span className="text-gray-700 ml-0.5">{text}</span>
-							<p className="-mt-1 text-gray-500 text-sm">{formatDistanceToNow(new Date(parseInt(createdAt)))}</p>
-						</div>
-					</div>
-
+				{console.log(data)}
+				{data && data.activities.map(({ id, user, text, profilePicture, createdAt}) => (
+					<Activity key={id} username={user.username} profilePicture={profilePicture} text={text} createdAt={createdAt} />					
 				))}
 				</div>
 			</Popover.Panel>
